8372812ee676b9c7679fa418b04f81345649bc08
xzz501 <xzz501@github.com>
2025-09-27 07:16:35 +1100

    صفحة التحقق من المستخدم الجديد

---
+++ _con.php
@@ -0,0 +1,114 @@
+<?php
+session_start();
+$eror=null;
+function chakuser(){
+    global $eror;
+    if (!empty($_POST['name'])){
+        $user=trim(htmlspecialchars($_POST['name'],ENT_QUOTES,'UTF-8'));
+        if(preg_match('/^[\p{Arabic}\s]{7,30}$/u',$user)){
+            return true;
+
+        }
+        else{
+            $eror="يرجى ادخال الاسم بشكل صحيح";
+            
+            // header('Location:singin.php');
+            $_SESSION['n']=1;
+            return false;
+        }
+    }
+    else{
+        $eror="يرجى ادخال الحقل الفارغ";
+        //header('Location:singin.php');
+        $_SESSION['n']=1;
+
+        return false;
+    }
+    
+}
+function chakpass(){
+    global $eror;
+    if(!empty($_POST['pass']) and !empty($_POST['pass1'])){
+        $pas=trim(htmlspecialchars($_POST['pass'],ENT_QUOTES,'UTF-8'));
+        $pass1=trim(htmlspecialchars($_POST['pass1'],ENT_QUOTES,'UTF-8'));
+        if(preg_match('/^[A-z0-9]{6,20}$/',$pas) and preg_match('/^[A-z0-9]{6,20}$/',$pass1)){
+            if($pas==$pass1){
+                return true;
+            }
+            else{
+                $eror="كلمات المرور غير متطابقة";
+                $_SESSION['n']=4;
+                return false;
+
+            }
+        }
+        else{
+            $eror="يجب ان تكون اكبر من 6 وتحتوي على حروف ورقام";
+            $_SESSION['n']=3;
+            return false;
+        }
+    }
+    else{
+        $eror="ادخل القيم الفارغه";
+        return false;
+    }
+}
+function chaemal(){
+    global $eror;
+    if(!empty($_POST['email'])){
+        $emal=trim(htmlspecialchars($_POST['email'],ENT_QUOTES,'UTF-8'));
+        if(filter_var($emal,FILTER_VALIDATE_EMAIL)){
+            return true;
+        }
+        else{
+            $eror="ادخل ايمل صحيح";
+            $_SESSION['n']=2;
+            return false;
+            
+        }
+    }
+    else {
+        $eror="ادخل الحقل الغارغ";
+         $_SESSION['n']=2;
+        return false;
+    }
+}
+function chack_phon_date(){
+    global $eror;
+    if(!empty($_POST['phon']) and !empty($_POST['data'])){
+        $phon=trim(htmlspecialchars($_POST['phon'],ENT_QUOTES,'UTF-8'));
+        if(preg_match('/^[0-9]{9}$/',$phon)){
+            return true;
+        } 
+        else {
+            $eror="ادخل ارقام";
+            $_SESSION['n']=5;
+            return false;
+        }
+    }
+    else{
+        $eror="ادخل القيمه الفارغة";
+        $_SESSION['n']=5;
+        return false;
+    }
+}
+if(chakuser() && chakpass() && chaemal()){
+$con=mysqli_connect('localhost','root','','animes');
+if(mysqli_connect_error()){
+    echo "error";
+}
+$name=$_POST['name'];
+$pho=intval($_POST['phon']);
+$em=$_POST['email'];
+$ps=$_POST['pass'];
+$da=$_POST['data'];
+mysqli_query($con,"INSERT INTO users(name,number,email,pass,data) VALUES('$name',$pho,'$em','$ps','$da');");
+mysqli_close($con);
+$_SESSION['loggedin']=true;
+header("Location:what's.php");
+}
+else{
+    $_SESSION['value']=$eror;
+    header('Location:singin.php');
+}
+?>
