7a50dd5b38eb9f29051e37a8575fb6913508b1b2
xzz501 <xzz501@github.com>
2025-09-27 06:41:26 +1100

    اضافة صفحة التحقق من المستخدم

---
+++ pho.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+
+$con=mysqli_connect('localhost','root','','animes');
+if(mysqli_connect_error())
+ echo "error";
+
+if(!empty($_POST['name']) and !empty($_POST['email']) and !empty($_POST['pass'])) {
+ $user=htmlspecialchars($_POST['name'],ENT_QUOTES,'UTF-8');
+ $email=htmlspecialchars($_POST['email'],ENT_QUOTES,'UTF-8');
+ $pass=htmlspecialchars($_POST['pass'],ENT_QUOTES,'UTF-8');
+ $r=mysqli_query($con,"SELECT * FROM users ");
+while($rr= mysqli_fetch_array($r)) {
+ if ($rr['name']===$user and $rr['email']===$email and $rr['pass']===$pass) {
+  $_SESSION['username']=$user;
+  $_SESSION['loggedin']=true;
+  if(isset($_POST['rem'])){
+    setcookie('username',"$user",time()+60*2);
+  }
+  header("Location: what's.php");
+} else {
+  header("Location:index.php");
+}
+}
+mysqli_close();
+} else{ 
+header("Location:index.php");
+}
+?>
